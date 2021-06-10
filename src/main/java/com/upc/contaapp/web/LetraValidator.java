92fda0fcf03f76c51d8aa75caf2ea4f1e2066c14
AntonioFerrandiz <u201918042@upc.edu.pe>
2021-06-09 20:15:04 -0500

    ...

---
+++ src/main/java/com/upc/contaapp/web/LetraValidator.java
@@ -0,0 +1,49 @@
+package com.upc.contaapp.web;
+
+import com.upc.contaapp.model.Letra;
+import com.upc.contaapp.service.LetraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class LetraValidator implements Validator{
+
+    @Autowired
+    private LetraService letraService;
+    @Override
+    public boolean supports(Class<?> aClass){ return Letra.class.equals(aClass);}
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Letra letra = (Letra) o;
+        //Valor Nominal
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "valornominal", "NotEmpty");
+        if(letra.getValorN() <= 0){
+            errors.rejectValue("valornominal", "value.letraForm.valornominal");
+        }
+        //Tasa Efectiva
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "tasaefectiva", "NotEmpty");
+        if(letra.getValorN() <= 0){
+            errors.rejectValue("tasaefectiva", "value.letraForm.tasaefectiva");
+        }
+        //Retención
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "retencion", "NotEmpty");
+        if(letra.getValorN() <= 0){
+            errors.rejectValue("retencion", "value.letraForm.retencion");
+        }
+        //Gastos Costos Iniciales
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "gastoscostosi", "NotEmpty");
+        if(letra.getValorN() <= 0){
+            errors.rejectValue("gastoscostosi", "value.letraForm.gastoscostosi");
+        }
+        //Gastos Costos Finales
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "gastoscostosf", "NotEmpty");
+        if(letra.getValorN() <= 0){
+            errors.rejectValue("gastoscostosf", "value.letraForm.gastoscostosf");
+        }
+    }
+
+}
